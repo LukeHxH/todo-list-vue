94c2c0deb5f708c6907e7311bab79e3266986bd0
lukehxh <lucasmed812@gmail.com>
2019-02-23 13:21:24 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <h1>{{title}}</h1>
+    
+    <input type="text" placeholder="Digite um item para adicionar na lista..."
+    v-model="content" @keyup.enter="addItem()" autofocus>
+
+    <ul v-for="(item, index) in todoList" v-bind:key="index">
+      <li>
+        <TodoItem v-bind:item="item" @remove="removeItem(index)">
+          Item {{item.index + 1}}
+        </TodoItem>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import TodoItem from './TodoItem.vue';
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      title: 'Lista de afazeres (to-do list)',
+      content: '',
+      todoList: []
+    }
+  },
+  components: { TodoItem },
+  methods: {
+    addItem() {
+      this.todoList.push({'description': this.content});
+      this.content = '';
+    },
+    removeItem(index) {
+      this.todoList.splice(index, 1);
+    }
+  }
+}
+</script>
---
+++ src/TodoItem.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+        <span>{{ item.description }}</span>
+        <button @click="$emit('remove')">remover</button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        item: Object
+    }
+}
+</script>
+
